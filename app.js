fd337526580003ebcfee731c19ca3412be86ddd3
Diego Flores <43457317+dflores13@users.noreply.github.com>
2023-03-24 14:55:53 -0700

    comments fixed on app.js

--- app.js
+++ app.js
@@ -3,33 +3,29 @@ const prompts = require('prompts');
 const api = require('./api.js'); 
 const history = require("./history.js");
 
-//gets characters information based on argument, runs a prompt after searching to select character.
+//gets character names based on argument, runs a prompt after searching to select character.
 const characterInformation = async(args) => {
     const characterName = args; 
     const findChar = await api.getWithQuery(characterName);
-    // let choices = _selectCharacterPrompt(findChar);
-
-    
-    // returns the data of the selected character
+    // stored the data of the selected character in const
     const selectedChar = await _selectCharacterPrompt(findChar);
-    
+    // neatly displays data of selected character
     const idk = filterData(selectedChar);
      
     // call history function with key = characterName, value = count of how many in search result
     const saveQuery = history.saveSearch(characterName, findChar);
 }; 
-
+// Gets character information by id and returns information
 const dataInformation = async(args) => {
     const characterData = args;
     const findData = await api.getWithId(characterData);
     return findData; 
 }
 
-
+// method neatly displays passed in data
 const filterData = async (data) => {
     const name = data.name;
     console.log("\nName: " + name);
-
     // films
     const film = data.films; 
     if(film[0] == null){
@@ -54,7 +50,6 @@ const filterData = async (data) => {
     }
     // tv shows
     const shows = data.films; 
-
     if(shows[0] == null){
         console.log("\nTV Shows: None ")
     }
@@ -66,7 +61,6 @@ const filterData = async (data) => {
     }
     // video games
     const games = data.videoGames; 
-
     if(games[0] == null){
         console.log("\nVideo Games: None ")
     }
@@ -78,7 +72,6 @@ const filterData = async (data) => {
     }
     // park attractions
     const park = data.parkAttractions; 
-
     if(park[0] == null){
         console.log("\nPark Attractions: None ")
     }
@@ -90,7 +83,6 @@ const filterData = async (data) => {
     }
     // allies
     const allies = data.allies; 
-
     if(allies[0] == null){
         console.log("\nAllies: None ")
     }
@@ -102,7 +94,6 @@ const filterData = async (data) => {
     }
     // enemies
     const enemies = data.enemies; 
-
     if(enemies[0] == null){
         console.log("\nEnemies: None ")
     }
@@ -113,19 +104,12 @@ const filterData = async (data) => {
     }) 
     }
 }; 
-
-
-// console.log(dataInformation("donald"));
-
-
 // search prompt
 // select from the list of characters based on their input
 const _selectCharacterPrompt = async (characters) => {
     const displayCharacter = characters.map((character) => {
         return {title : `${character.name}`, value: character._id}
     });
-
-
     const response = await prompts([
         {
             type: 'select',
@@ -134,11 +118,8 @@ const _selectCharacterPrompt = async (characters) => {
             choices: displayCharacter,
         }
     ]);
-    // console.log(response.characters[0])
-    //displays data for character
-    // console.log(dataInformation(response.characters));
+    //returns character data information from prompt response
     return dataInformation(response.characters); 
-
 };
 
 module.exports = {
