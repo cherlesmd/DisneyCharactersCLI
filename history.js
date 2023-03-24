28120424c3d26ca8dcd00dc46cc6bb2b51be0a8e
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2023-03-24 06:03:15 -0700

    functioning json writer

--- history.js
+++ history.js
@@ -1,23 +1,34 @@
-const fs = require("fs");
-const data = require("./data.json");
-
-
-// save previous queries
-// count how many results per query
-// in the form of an array of objects -  key "search" and the key "resultCount"
-
+const fs = require('fs');
 
 const saveSearch = (searchName, results) => {
 
     const resultCount = results.length;
+    const initData = {};
+    initData[searchName] = resultCount;
+    const initArr = [initData];
 
-    fs.writeFile("data.json", JSON.stringify(`{${searchName}: ${resultCount}}`), (error) => {
-        if (error) throw error;
-        console.log("Done wrting...");
+    const saveJSON = async(inData) => {
+        try {
+            await fs.promises.writeFile("./data.json", inData)
+    
+            console.log("File written successfully");
+    
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    fs.promises.readFile("./data.json")
+    .then((success) => {
+        // parse data, then send to saveJson
+        const adjustArray = JSON.parse(success);
+        adjustArray.push(initData);
+        saveJSON(JSON.stringify(adjustArray, null, 2));
+
+    }).catch((err) => {
+        // if no exisiting file, create new file
+        saveJSON(JSON.stringify(initArr, null, 2));
     });
-
-    // const newData = {
-    // };
 };
 
 module.exports = {
